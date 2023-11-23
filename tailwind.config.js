9ff3d3a42e476e02b58c2e617847196c371a1cdd
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-11-23 20:46:00 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
       backgroundImage: {
         "steps-bg": "url(./src/assets/images/bg-sidebar-desktop.svg)",
       },
+      backgroundColor: {
+        body: "hsl(217, 100%, 97%)",
+        "btn-next": " hsl(213, 96%, 18%)",
+      },
+
+      textColor: {
+        "step-title": "hsl(229, 24%, 87%)",
+        "form-label": " hsl(213, 96%, 18%)",
+      },
     },
   },
   plugins: [],
